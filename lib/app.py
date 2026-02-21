2bda76461338313726642a54659a6846987201af
michaelfuchaka <fuchakamichael06@gmail.com>
2026-02-21 16:46:17 +0300

    Uncomment print statements in app.py to enable output

--- lib/app.py
+++ lib/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 print("Hello World! Pass this test, please.")
-# print("Hello sun!")
-# print("Hello sky")
+print("Hello sun!")
+print("Hello sky")
 
 
-# print("Hello world!", end=" ")
-# print("Hello sun!", end="!! ")
-# print("Hello sky!", end="!!!\n")
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
